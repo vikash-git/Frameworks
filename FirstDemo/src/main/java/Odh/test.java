3111bcb438e94dc0ed69cd583c06d9cd7bfb2cde
Vikash <Vikash@Vipul-PC.mshome.net>
2019-06-12 04:18:50 +0530

    @read and create file

---
+++ FirstDemo/src/main/java/Odh/test.java
@@ -0,0 +1,143 @@
+package Odh;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+public class test {
+
+	public static String source_path="C:\\Users\\Vikash\\Downloads\\upLOAD\\test_source\\";
+	public static String dest_path="C:\\Users\\Vikash\\Downloads\\upLOAD\\test_dest\\";
+	public static String excl_path="C:\\Users\\Vikash\\Downloads\\upLOAD\\test_source\\data.xls";
+	public static File file = new File(source_path);
+	public static String[] fileList = file.list();
+	 public static List<String> file_list = new ArrayList<String>(fileList.length);
+	public static void main(String[] args) throws BiffException, IOException 
+	{	
+	 
+		arrayTOlist();
+    
+		File inputWorkbook = new File(excl_path);
+		Workbook w;
+        w = Workbook.getWorkbook(inputWorkbook);
+        // Get the first sheet
+        Sheet sheet = w.getSheet(0);
+	
+        for (int i = 0; i < sheet.getRows(); i++) 
+            
+		{ int count =0;
+			
+			String f_name=sheet.getCell(0, i).getContents();
+			String event_id=sheet.getCell(1, i).getContents();
+			
+			 for (String temp_filename:fileList)
+			 {		 
+				if(temp_filename.contains(f_name)&&temp_filename.contains(event_id))
+				{
+					count++;
+				}
+				
+			 }
+			 if(count!=0)
+			 {
+				// System.out.println("file found "+"filename="+f_name+" with event id "+event_id);
+				 System.out.println(f_name +"\t"+event_id+"\t"+"--Found");
+			 }
+			 else 
+				 //System.out.println("not  found "+"filename="+f_name+" with event id "+event_id);
+				 System.out.println(f_name +"\t"+event_id+"\t"+"--Not Found");
+		}
+        
+        
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private static void arrayTOlist() 
+	{
+		
+	       
+	        
+	        for (String s:fileList) {
+	        	file_list.add( s );
+	        }
+	       
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+	public static void create_psv_and_tcf_files () 
+	{
+	
+		 
+	        
+	        for(String name:fileList)
+	        {
+	        	if (name.contains(".appd"))
+	        	{	
+	        	Writer writer = null;
+	        	
+				
+				  File oldfile =new File(source_path+name); System.out.println(oldfile);
+				  
+				  File newfile =new File(dest_path+name.replaceAll(".appd", ".psv"));
+				  
+				  if(oldfile.renameTo(newfile)){ System.out.println("File renamed"); }else{
+				  System.out.println("Sorry! the file can't be renamed already present in destinaton"); }
+				 
+	 	       
+
+	 	      try {
+	 	          writer = new BufferedWriter(new OutputStreamWriter(
+	 	                new FileOutputStream(dest_path+name.replace(".appd",".tcf")), "utf-8"));
+	 	          writer.write(name.replaceAll(".appd", ".psv"));
+	 	      } catch (IOException ex) {
+	 	          // Report
+	 	      } finally {
+	 	         try {writer.close();} catch (Exception ex) {/*ignore*/}
+	 	      }
+	 	        
+	 	        
+	 	        
+	 	        
+	 	        
+	        }}}}
+	        
+	       
+	        
+	        
+	        
+	
+
+	
+
+
