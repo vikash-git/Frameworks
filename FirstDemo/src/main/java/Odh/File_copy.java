8b9f4b02c7ac0b5f3f37358645d7de5bfb242c5b
Vikash <Vikash@Vipul-PC.mshome.net>
2019-07-02 02:38:12 +0530

    latest Working

--- FirstDemo/src/main/java/Odh/File_copy.java
+++ FirstDemo/src/main/java/Odh/File_copy.java
@@ -6,9 +6,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
-
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
@@ -17,15 +20,18 @@ public class File_copy {
 
 	public static String source_path="C:\\Users\\Vikash\\Downloads\\upLOAD\\test_source\\";
 	public static String dest_path="C:\\Users\\Vikash\\Downloads\\upLOAD\\test_dest\\";
-	public static String excl_path="C:\\Users\\Vikash\\Downloads\\upLOAD\\test_source\\data.xls";
+	public static String usr_dir=null;
+	public static String excl_path=null;
 	public static File file = new File(source_path);
 	public static String[] fileList = file.list();
 	public static String[] fileList2 =new String[file.list().length];
 
 	 public static List<String> file_list = new ArrayList<String>(fileList.length);
 	public static void main(String[] args) throws BiffException, IOException 
-	{	
-	 
+		{	   
+		
+		usr_dir = System.getProperty("user.dir");
+		String excl_path=usr_dir+"\\data.xls";
 		arrayTOlist();
 		int z=0;
 		File inputWorkbook = new File(excl_path);
@@ -64,12 +70,13 @@ public class File_copy {
 		}
         
         
-        for (String s:fileList2) {
-        	if (s!=null)
-        	System.out.println(s);
-        }
-        
-       //create_psv_and_tcf_files();
+		/*
+		 * for (String s:fileList2) { if (s!=null) System.out.println(s); }
+		 */
+        System.out.println("");
+        System.out.println("=====================================");
+        System.out.println("");
+       create_psv_and_tcf_files();
 		
 	}
 	
@@ -100,32 +107,47 @@ public class File_copy {
 
 
 
+	 public static void copyFile(String from, String to) throws IOException{
+	        Path src = Paths.get(from);
+	        Path dest = Paths.get(to);
+	        Files.copy(src, dest,StandardCopyOption.REPLACE_EXISTING);
+	        System.out.println("Copy From = "+from );
+	        System.out.println("Copy T0 = "+to);
+	        System.out.println("");
+	        
+	    }
 
 
 
 
 
 
-
-	public static void create_psv_and_tcf_files () 
+	public static void create_psv_and_tcf_files () throws IOException 
 	{
 	
 		 
 	        
 	        for(String name:fileList2)
 	        {
+	        	if(name!=null)
 	        	if (name.contains(".appd"))
 	        	{	
 	        	Writer writer = null;
+	        	//copyFile(source_path+name,dest_path+name.replaceAll(".appd", ".psv"));
+	        	copyFile(source_path+name,dest_path+name);
 	        	
-				
-				  File oldfile =new File(source_path+name); System.out.println(oldfile);
-				  
-				  File newfile =new File(dest_path+name.replaceAll(".appd", ".psv"));
-				  
-				  if(oldfile.renameTo(newfile)){ System.out.println("File renamed"); }else{
-				  System.out.println("Sorry! the file can't be renamed already present in destinaton"); }
-				 
+	        	/*
+				 * File oldfile =new File(source_path+name); System.out.println(oldfile);
+				 * 
+				 * File newfile =new File(dest_path+name.replaceAll(".appd", ".psv"));
+				 * 
+				 * if(oldfile.renameTo(newfile)) { System.out.println("File renamed"); }
+				 * 
+				 * else{ System.out.
+				 * println("Sorry! the file can't be renamed already present in destinaton"); }
+				 */
+	        	
+	        	
 	 	       
 
 	 	      try {
